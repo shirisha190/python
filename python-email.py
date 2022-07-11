49039d6ddb69a93240275d7a1de946f012094543
shirisha190 <shirisha190@gmail.com>
2022-07-11 09:07:13 -0600

    update

--- python-email.py
+++ python-email.py
@@ -5,8 +5,8 @@ from email.message import EmailMessage
 
 msg = EmailMessage()
 msg['subject'] = 'python test 2 '
-msg['From'] = 'jaipalkomakula@gmail.com'
-msg['To'] = 'jaipalk.devops@gmail.com'
+msg['From'] = ''
+msg['To'] = ''
 msg.set_content('this is a python test 2')
 
 #EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
@@ -16,17 +16,17 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     #smtp.starttls()
     #smtp.ehlo()
 
-    smtp.login('jaipalkomakula@gmail.com','tovabbfzzopiggljaipal123')
+    smtp.login('','')
 
 #    subject = 'python test email'
 #    body = 'Python test email -- for tesing'
 
 #    msg = f"Subject: {subject}\n\n{body}"
 
-#    smtp.sendmail('jaipalkomakula@gmail.com', 'jaipalk.devops@gmail.com' , msg)
+#    smtp.sendmail('', '' , msg)
     smtp.send_message(msg)
 
-#jaipalkomakula@gmail.com
+#jaiaplgmail.com
 #tovabbfzzopiggljjaipal123
 #http://www.cyberkeeda.com/2020/06/python-send-html-table-as-email-body.html
 
